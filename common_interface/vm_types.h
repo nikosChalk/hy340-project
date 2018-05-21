b9d3ce0239457f5396adcc36bed056c0091eb93e
georgeepta <giwrgosepta1997@hotmail.com>
2018-05-22 01:26:59 +0300

    some adds for target_code generation

--- common_interface/vm_types.h
+++ common_interface/vm_types.h
@@ -130,6 +130,21 @@ namespace virtual_machine {
          */
         Userfunc(unsigned int address, unsigned int nr_locals, const std::string &name);
     };
+
+	struct Incomplete_Jump{
+		unsigned int instrNo;
+		unsigned int iaddress;
+		/**
+		* Default initialization. (Junk)
+		*/
+		Incomplete_Jump();
+		/**
+		* Initializes the Incomplete_Jump representation with the given attributes
+		* @param nr_locals The total number of local variables used by this function
+		* @param name The function's name
+		*/
+		Incomplete_Jump(unsigned int instrNo, unsigned int iaddress);
+	};
 };
 
 #endif //HY340_PROJECT_VM_TYPES_H
