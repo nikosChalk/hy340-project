b69edac17ff11e0a879b1ed51a1928cae30d3d16
nikosChalk <chalkiadakisn@gmail.com>
2018-05-25 17:05:09 +0300

    Meiwsei kwdika twn math library functions

--- virtual_machine/AVM/builtin_funcs/math.cpp
+++ virtual_machine/AVM/builtin_funcs/math.cpp
@@ -1,11 +1,16 @@
-#include <math.h>
+
+
+#include <cmath>
 #include "../AVM.h"
 #include "../errors/internal_error.h"
 
-#define PI 3.14159265
-
 using namespace std;
 using namespace virtual_machine;
+
+/**
+ * Converts the given number from degrees to rads
+ */
+#define to_rads(x) ( ((x) * M_PI) / 180.0)
 
 void AVM::libfunc_sqrt(void) {
 	unsigned int args = program_stack.get_total_actuals();
@@ -13,19 +18,18 @@ void AVM::libfunc_sqrt(void) {
 	if (args != 1)
 		throw internal_error("Expected 1 argument in library function 'sqrt' but got " + std::to_string(args));
 
-	if (program_stack.get_actual_arg(0)->type != Memcell::Type::number)
+	Memcell const *first_arg = program_stack.get_actual_arg(0);
+	if (first_arg->type != Memcell::Type::number)
 		throw internal_error("Argument type of 'sqrt' must be a number");
-	else{
-		if (program_stack.get_actual_arg(0)->value.num < 0){
-			retval.clear(); //clear register's content
-			retval.type = Memcell::Type::nil;
-		}
-		else{
-			retval.clear(); //clear register's content
-			retval.type = Memcell::Type::number;
-			retval.value.num = pow(program_stack.get_actual_arg(0)->value.num, 1 / 2);
-		}
-	}
+
+	//Sqrt can be calculated
+    retval.clear(); //clear register's content
+    if(first_arg->value.num < 0) {
+        retval.type = Memcell::Type::nil;
+    } else {
+        retval.type = Memcell::Type::number;
+        retval.value.num = pow(first_arg->value.num, 0.5);
+    }
 }
 
 void AVM::libfunc_cos(void) {
@@ -34,13 +38,14 @@ void AVM::libfunc_cos(void) {
 	if (args != 1)
 		throw internal_error("Expected 1 argument in library function 'cos' but got " + std::to_string(args));
 
-	if (program_stack.get_actual_arg(0)->type != Memcell::Type::number)
+    Memcell const *first_arg = program_stack.get_actual_arg(0);
+	if (first_arg->type != Memcell::Type::number)
 		throw internal_error("Argument type of 'cos' must be a number");
-	else{
-		retval.clear(); //clear register's content
-		retval.type = Memcell::Type::number;
-		retval.value.num = cos(program_stack.get_actual_arg(0)->value.num * PI / 180.0);
-	}
+
+
+    retval.clear(); //clear register's content
+    retval.type = Memcell::Type::number;
+    retval.value.num = cos(to_rads(first_arg->value.num));
 }
 
 void AVM::libfunc_sin(void) {
@@ -49,11 +54,12 @@ void AVM::libfunc_sin(void) {
 	if (args != 1)
 		throw internal_error("Expected 1 argument in library function 'sin' but got " + std::to_string(args));
 
-	if (program_stack.get_actual_arg(0)->type != Memcell::Type::number)
+    Memcell const *first_arg = program_stack.get_actual_arg(0);
+	if (first_arg->type != Memcell::Type::number)
 		throw internal_error("Argument type of 'sin' must be a number");
-	else{
-		retval.clear(); //clear register's content
-		retval.type = Memcell::Type::number;
-		retval.value.num = sin(program_stack.get_actual_arg(0)->value.num * PI / 180.0);
-	}
+
+
+    retval.clear(); //clear register's content
+    retval.type = Memcell::Type::number;
+    retval.value.num = sin(to_rads(first_arg->value.num));
 }
